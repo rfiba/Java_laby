9f80600b9ce43976b4e8dc0ae2ca85314a85518a
rfiba <rafal.fiba@hotmail.com>
2018-03-07 18:00:56 +0100

    ItemCondition gettery i increase(), FullfilmentCenter - addProduct(), getProduct() - rozpoczęcie

--- Apka_lab2/src/com/company/FulfillmentCenter.java
+++ Apka_lab2/src/com/company/FulfillmentCenter.java
@@ -1,4 +1,43 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class FulfillmentCenter {
+    private String name;
+    private List<Item> itemList;
+    private double capacity;
+    private double currentWeight;
+
+    FulfillmentCenter(String nameToAdd, double capacityToAdd) {
+        if (capacityToAdd <= 0)
+            throw new IllegalArgumentException("Argument <= 0");
+
+        name = nameToAdd;
+        capacity = capacityToAdd;
+        currentWeight = 0;
+        List<Item> itemList = new LinkedList<>();
+    }
+
+    void addProduct(Item toAdd) {
+        if (currentWeight + toAdd.getWeight() <= capacity)
+            throw new IllegalArgumentException("Cannot add. Lack of capacity");
+
+        int it = itemList.indexOf(toAdd);
+
+        if (it == -1) {
+            itemList.add(toAdd);
+        } else {
+            itemList.get(it).increaseWeight(toAdd.getWeight());
+            itemList.get(it).increase();
+        }
+
+    }
+
+    Item getProduct(Item toGet) {
+        int it = itemList.indexOf(toGet);
+        if (it == -1)
+            throw new IllegalArgumentException("Cannot get. Item unfound");
+        
+    }
 }
