a6b252033a7ba766090b4ac79ee35e6cf6e23106
rfiba <rafal.fiba@hotmail.com>
2018-03-08 10:04:14 +0100

    poprawa Item

---
+++ Apka_lab2/src/com/company/Item.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Item implements java.lang.Comparable<Item>{
+    private String name;
+    private ItemCondition state;
+    private double weight;
+    private int amount;
+
+    Item(String nameToAdd, ItemCondition stateToAdd, double weightToAdd, int amountToAdd) {
+        if (weightToAdd < 0 || amountToAdd < 0)
+            throw new IllegalArgumentException("Argument < 0");
+
+        name = nameToAdd;
+        state = stateToAdd;
+        weight = weightToAdd;
+        amount = amountToAdd;
+    }
+
+    void print() {
+        System.out.format("Name: %s, weight: %f, amount: %d " + state, name, weight, amount);
+    }
+
+    double getWeight() {
+        return weight;
+    }
+    void increaseWeight(double weightToAdd) {
+        weight += weightToAdd;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    void increase() {
+        amount++;
+    }
+
+    void decrease() {
+        amount--;
+    }
+
+    @Override
+    public int compareTo(Item o) {
+            return name.compareTo(o.name);
+    }
+}
