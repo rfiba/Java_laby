e7d4252bbe957ae16c0c4169eef047ca0498235e
rfiba <rafal.fiba@hotmail.com>
2018-04-03 10:27:23 +0200

    2 testy dla MyList

--- Algorithm_lab5/src/com/company/MyListTest.java
+++ Algorithm_lab5/src/com/company/MyListTest.java
@@ -2,11 +2,27 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyListTest {
 
     @Test
-    void solution() {
+    void solution_NullList_ThrownException() {
+        NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+            MyList.solution(null);
+        });
+        assertEquals("List is null", exception.getMessage());
+    }
+
+    @Test
+    void solution_EmptyList_ThrownException() {
+        List<Integer> list = new ArrayList<>();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            MyList.solution(list);
+        });
+        assertEquals("List is empty", exception.getMessage());
     }
 }
