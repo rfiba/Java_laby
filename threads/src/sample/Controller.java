6becf6a4ca8371fafcd17c0c6ca6578ac38848d7
rfiba <rafal.fiba@hotmail.com>
2018-04-14 12:12:18 +0200

    Controller klasa dodana

--- threads/src/sample/Controller.java
+++ threads/src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
 public class Controller {
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private void handleRunBtnAction(){
+        GraphicsContext gc =
+                canvas.getGraphicsContext2D(); drawShapes(gc);
+    }
+
+    private void drawShapes(GraphicsContext gc)
+    { gc.setFill(Color.BLUEVIOLET);
+        gc.fillRect(gc.getCanvas().getLayoutX(),
+                gc.getCanvas().getLayoutY(),
+                gc.getCanvas().getWidth(),
+                gc.getCanvas().getHeight());
+    }
 }
