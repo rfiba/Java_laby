4112763bb7c179da3eb650308419873596bd7da1
rfiba <rafal.fiba@hotmail.com>
2018-04-15 18:58:38 +0200

    stworzenie klasy PointEvent

---
+++ threads/src/sample/PointEvent.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class PointEvent {
+    double x;
+    double y;
+    boolean belongingness;
+
+    public PointEvent(double xToAdd, double yToAdd, boolean belongingnessToAdd){
+        x = xToAdd;
+        y = yToAdd;
+        belongingness = belongingnessToAdd;
+    }
+}
+
